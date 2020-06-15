168bb9293bd9487926b16952755cbd6c2b2f663e
Shaikh Vazid <shaikh.vazid@tensult.com>
2020-06-15 07:54:25 +0530

    remove state files

--- terraform-configs/elasticsearch/output.tf
+++
@@ -1,9 +0,0 @@
-output "endpoint" {
-  value       = "${aws_elasticsearch_domain.voice-services-es.endpoint}"
-  description = "Elasticsearch Domian "
-}
-
-output "kibana_endpoint" {
-  value       = "${aws_elasticsearch_domain.voice-services-es.kibana_endpoint}"
-  description = "Kibana Endpoints"
-}
